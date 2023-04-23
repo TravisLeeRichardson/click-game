8c1dd838e3999aab7813c65409c6af5fcf7b8ab9
Travis <travis@nervos.org>
2023-04-22 22:44:49 -0400

    Added a couple new features

--- scripts.js
+++ scripts.js
@@ -4,8 +4,12 @@ const resetScore = document.getElementById("resetScore");
 const result = document.getElementById("result");
 const startGame = document.getElementById("startGame");
 const timer = document.getElementById("timer")
+const INSTRUCTIONS_TEXT = "Click the white oval as many times as you can before the timer runs out!";
+
 
 let timeLimit = 10000;
+let timeoutId;
+let intervalId;
 
 const shuffleColors = () => {
   const colors = ["red", "blue", "white", "orange", "yellow", "green"];
@@ -18,19 +22,57 @@ const shuffleColors = () => {
 }
 
 const endGame = () => {
-  startGame.style.visibility = "visible";
+  
   buttons.forEach(button => {
     button.removeEventListener("click", handleClick);
     button.style.backgroundColor = "black";
+    if (["two", "five"].includes(button.getAttribute("id"))) {
+      button.style.backgroundColor = "white"
+    }
+
+    // Show very important message
+    const winnerBubble = document.getElementById("two");
+    winnerBubble.style.backgroundImage = 'url("https://gateway.pinata.cloud/ipfs/QmexS9rTrrVxk4QAGceMLbAs2etfVgrBn61nDG9iLPUG25?_gl=1*nketmy*rs_ga*MDMwODZjN2QtZjEyYi00Mjk5LTkxOTctZDA4MGE1MjM1YjZl*rs_ga_5RMPXG14TE*MTY4MjIxNzYwNi41LjEuMTY4MjIxNzYyNS40MS4wLjA.")';
+    winnerBubble.style.backgroundSize = "cover";
+    winnerBubble.style.backgroundPosition = "center top";
+
+    // Show the legend 
+    if (button.getAttribute("id") === "five") {
+      const legendBubble = document.getElementById("five");
+      legendBubble.style.backgroundImage = 'none';
+      legendBubble.style.backgroundColor = 'white';
+      legendBubble.style.textAlign = 'center';
+      legendBubble.style.display = 'flex';
+      legendBubble.style.justifyContent = 'center';
+      legendBubble.style.alignItems = 'center';
+      legendBubble.innerHTML = '<div style="width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center;"><h3 style="margin: 5px 0; font-style: italic;">(Legend below by master Clicker, Kiernan Richardson)</h3><p style="margin: 10px 0;">0-5 = Noob</p><p style="margin: 10px 0;">6-10 = Still Kind of a Noob</p><p style="margin: 10px 0;">11-15 = Ok, Ok, You have Skills</p><p style="margin: 10px 0;">16-20 = You put the game in a headlock</p><p style="margin: 10px 0;">21+ = You took the red pill.</p></div>';
+    }
+
   });
+
   clearTimeout(timeoutId);
   timer.textContent = "";
+  const score = parseInt(points.textContent);
 };
 
-
+const assignColors = () => {
+  const shuffledColors = shuffleColors();
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.backgroundColor = shuffledColors[i];
+    buttons[i].style.backgroundImage = 'none';
+    buttons[i].innerHTML = '';
+    buttons[i].addEventListener("click", handleClick);
+  };
+}
 const resetScoreFunction = () => {
   points.textContent = "0 points";
-  endGame();
+  assignColors();
+  startGame.style.visibility = "visible";
+  clearTimeout(timeoutId);
+  clearInterval(intervalId);
+  remainingTime = timeLimit;
+  timer.textContent = "";
+  const score = parseInt(points.textContent);
 }
 
 const handleClick = (event) => {
@@ -49,15 +91,10 @@ const handleClick = (event) => {
 const startGameFunction = () => {
   startGame.style.visibility = "hidden";
   points.textContent = "0 points";
-  const shuffledColors = shuffleColors();
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundColor = shuffledColors[i];
-    buttons[i].addEventListener("click", handleClick);
-  };
 
-  let remainingTime = timeLimit;
+   remainingTime = timeLimit;
   timeoutId = setTimeout(endGame, timeLimit);
-  const intervalId = setInterval(() => {
+   intervalId = setInterval(() => {
     remainingTime -= 10;
     const seconds = Math.floor(remainingTime / 1000);
     const milliseconds = (remainingTime % 1000)
@@ -71,7 +108,6 @@ const startGameFunction = () => {
     }
   }, 10);
 };
-
 
 resetScore.addEventListener("click", resetScoreFunction);
 startGame.addEventListener("click", startGameFunction)
